4ec02ba5d630ee37ec922ce739b14110c2af6f3c
KealPetu <kpenafiel99@gmail.com>
2026-02-24 15:30:44 -0500

    Modified the Player struct implementation

--- main.cpp
+++ main.cpp
@@ -1,43 +1,43 @@
-#include <iostream>
-#include <raylib.h>
-
-#include "build/_deps/raylib-src/src/raymath.h"
+#include "raylib.h"
+#include "raymath.h"
 
 constexpr int WINDOW_WIDTH  {800};
 constexpr int WINDOW_HEIGHT {450};
 constexpr float GRAVITY_ACC {981.f};
 
 struct Player {
-    Vector2 position    {0.f,0.f};
-    Vector2 velocity    {0.f,0.f};
-    Vector2 size        {0.f,0.f};
-    float jumpVelocity  {0.f};
+    Vector2 velocity    {0.f,0.f };
+    Rectangle rectangle {0.f, 0.f, 0.f, 0.f };
+    Texture2D texture   {0, 0, 0, 0, 0 };
+    float jumpVelocity  { 0.f };
     bool canJump        {false};
 };
 
 int main() {
+    SetConfigFlags(FLAG_VSYNC_HINT);
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "dapper dasher game");
 
     float deltaTime {0.f};
     Player player {
-        Vector2{0.f, 0.f},
-        Vector2{0.f,0.f},
-        Vector2{50.f,80.f},
-        654.f,
-        false,
+        Vector2 {0.f,0.f},
+        Rectangle {0, 0, 0, 0},
+        Texture2D { LoadTexture("resources/sprites/scarfy/scarfy.png") },
+        GRAVITY_ACC * 2 / 3,
+        false
     };
 
-    player.position.x = WINDOW_WIDTH/2.f - player.size.x / 2.f;
-    SetConfigFlags(FLAG_VSYNC_HINT);
-    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "dapper dasher game");
+    player.rectangle.width = player.texture.width/6;
+    player.rectangle.height = player.texture.height;
+    player.rectangle.x = WINDOW_WIDTH/2.f - player.rectangle.width / 2.f;
 
     while (!WindowShouldClose()) {
 
         deltaTime = GetFrameTime();
         player.velocity.y += GRAVITY_ACC * deltaTime;
-        player.position.y += player.velocity.y * deltaTime;
-        player.position.y = Clamp(player.position.y, 0.f, WINDOW_HEIGHT - player.size.y);
+        player.rectangle.y += player.velocity.y * deltaTime;
+        player.rectangle.y = Clamp(player.rectangle.y, 0.f, WINDOW_HEIGHT - player.rectangle.height);
 
-        if (player.position.y == WINDOW_HEIGHT - player.size.y) {
+        if (player.rectangle.y == WINDOW_HEIGHT - player.rectangle.height) {
             player.canJump = true;
             player.velocity.y = 0.f;
         }
@@ -49,8 +49,8 @@ int main() {
 
         BeginDrawing();
             ClearBackground(WHITE);
-            DrawRectangle(static_cast<int>(player.position.x), static_cast<int>(player.position.y),
-                          static_cast<int>(player.size.x), static_cast<int>(player.size.y), BLUE);
+            DrawRectangle(static_cast<int>(player.rectangle.x), static_cast<int>(player.rectangle.y),
+                          static_cast<int>(player.rectangle.width), static_cast<int>(player.rectangle.height), BLUE);
             DrawText(TextFormat("player.velocity.y: %.2f", player.velocity.y), 10, 10, 10, RED);
         EndDrawing();
     }
