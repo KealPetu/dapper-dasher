db31e24ea1508bafedd5b3c1a39d4600b815892a
KealPetu <kpenafiel99@gmail.com>
2026-02-24 14:01:24 -0500

    changed names of variables and code structure

--- main.cpp
+++ main.cpp
@@ -3,17 +3,16 @@
 
 #include "build/_deps/raylib-src/src/raymath.h"
 
-constexpr int WINDOW_WIDTH  { 800 };
-constexpr int WINDOW_HEIGHT { 450 };
-constexpr float GRAVITY_ACC { 981.f };
+constexpr int WINDOW_WIDTH  {800};
+constexpr int WINDOW_HEIGHT {450};
+constexpr float GRAVITY_ACC {981.f};
 
 struct Player {
-    Vector2 position {0.f,0.f};
-    Vector2 velocity {0.f,0.f};
-    Vector2 size {0.f,0.f};
-    bool canJump {false};
-    float jumpSpeed {0.f};
-    const float MAXIMUM_VELOCITY { 10.f };
+    Vector2 position    {0.f,0.f};
+    Vector2 velocity    {0.f,0.f};
+    Vector2 size        {0.f,0.f};
+    float jumpVelocity  {0.f};
+    bool canJump        {false};
 };
 
 int main() {
@@ -23,13 +22,12 @@ int main() {
         Vector2{0.f, 0.f},
         Vector2{0.f,0.f},
         Vector2{50.f,80.f},
+        654.f,
         false,
-        654.f
     };
 
     player.position.x = WINDOW_WIDTH/2.f - player.size.x / 2.f;
-
-    //SetConfigFlags(FLAG_VSYNC_HINT);
+    SetConfigFlags(FLAG_VSYNC_HINT);
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "dapper dasher game");
 
     while (!WindowShouldClose()) {
@@ -37,7 +35,6 @@ int main() {
         deltaTime = GetFrameTime();
         player.velocity.y += GRAVITY_ACC * deltaTime;
         player.position.y += player.velocity.y * deltaTime;
-
         player.position.y = Clamp(player.position.y, 0.f, WINDOW_HEIGHT - player.size.y);
 
         if (player.position.y == WINDOW_HEIGHT - player.size.y) {
@@ -46,13 +43,14 @@ int main() {
         }
 
         if (IsKeyPressed(KEY_SPACE) && player.canJump) {
-            player.velocity.y = -player.jumpSpeed;
+            player.velocity.y = -player.jumpVelocity;
             player.canJump = false;
         }
 
         BeginDrawing();
             ClearBackground(WHITE);
-            DrawRectangle(player.position.x, player.position.y, player.size.x, player.size.y, BLUE);
+            DrawRectangle(static_cast<int>(player.position.x), static_cast<int>(player.position.y),
+                          static_cast<int>(player.size.x), static_cast<int>(player.size.y), BLUE);
             DrawText(TextFormat("player.velocity.y: %.2f", player.velocity.y), 10, 10, 10, RED);
         EndDrawing();
     }
